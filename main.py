6f0724e70abca0b9ee8c6627ad6b2a5dd895c52d
Dhruv Raval <draval9230@gmail.com>
2023-02-04 12:07:17 -0600

    version works

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from flask import Flask, render_template, request
-import test
+import DescriptionMatch
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    if request.method == "POST":
-        if request.form.get('action1') == 'VALUE1':
-           print("hi")
-    return render_template('index.html')
+    return render_template('UI.html')
 
+@app.route('/',methods =['POST'])
+def getValue():
+    #name, value
+    if request.form.get('action1') == 'Submit':
+        jobdescription = request.form.get('jobdescription')
+        resumeText = request.form.get('resumepaste')
+        DescriptionMatch.takeInput(resumeText, jobdescription)
+    return render_template('ui.html')
 
 if __name__ == '__main__':
     # test.testing()
